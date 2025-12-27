ab9453e16a1239d5c507a57597583c604dbe8db9
4rdhiya5hita <ardhiya.shita007@student.unud.ac.id>
2025-12-27 14:21:08 +0800

    coba

--- views/layout/header.php
+++ views/layout/header.php
@@ -12,15 +12,7 @@
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-<<<<<<< HEAD:layout/header.php
-    <!-- link css datatable -->
-    <link href="https://cdn.datatables.net/2.3.5/css/dataTables.dataTables.min.css" rel="stylesheet">
-    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
-    
-    <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-=======
     <link href="<?= BASE_URL ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
->>>>>>> b0f50d7f3d28cc26917576129a35c4649ac07365:views/layout/header.php
 
     <link href="<?= BASE_URL ?>assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
